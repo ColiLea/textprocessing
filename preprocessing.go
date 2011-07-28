ae6c501510bf61bc9ca62fc4e1a29bf75a4e22ed
Lea Frermann <l1@frermann.de>
2011-07-28 13:16:42 +0200

    Interface for SuffixArrays (-> must implement Less function); Less() function for Text; generic Less() function for SuffixArray; SuffixString() function for Text (returns joint list of all suffixes); updated String() function for SuffixArray sorts suffix-list returned by SuffixString() wrt to suffixArray (-> alphabetically)

--- preprocessing.go
+++ preprocessing.go
@@ -1,30 +1,65 @@
 package textprocessing
 
-//import "fmt"
 import "strings"
-//import "sort"
 import "io/ioutil"
 import "regexp"
-// import "math"
+//import "fmt"
 
 
 type Word string
 
 type Text []Word
 
-func NewTextFile(filename string) (text Text) {
+func NewTextFile(filename string) (text *Text) {
     rawTextByte,_ := ioutil.ReadFile(filename)
     text = NewText(string(rawTextByte))
     return
 }
 
-func NewText(str string) (text Text) {
+func NewText(str string) (text *Text) {
     const punct = "[?!.,;:^/\\<>//(//)\"\t\n]*"
     reStrip := regexp.MustCompile(punct+`[ \t\n]+`+punct)
     textStringSlice := strings.Split(reStrip.ReplaceAllString(str, " "), " ", -1)
-    text = make([]Word,len(textStringSlice))
+    slice := make(Text,len(textStringSlice))
+    text = &slice
     for idx,word := range textStringSlice {
-        text[idx]=Word(strings.ToLower(word))
+        (*text)[idx]=Word(strings.ToLower(word))
         }
     return
+}
+
+func (text *Text) Length() (int) {
+	return len(*text)
+}
+
+func (text Text) Less(length, m, n int) (bool) {
+        for{
+                switch {
+                        case text[m]<text[n] :
+                                return true
+                        case text[m]>text[n] :
+                                return false
+                        case text[m]==text[n] :
+                                m++
+                                n++
+                                if n == length {
+                                        return false
+                                } else if m == length {
+                                        return true
+                                }
+                }
+        }
+        panic("unreachable")
+}
+
+func (text *Text) SuffixString() (string){
+	suffixes := make([]string, text.Length())
+	for idx,_ := range *text {
+		tmp := make([]string, (*text).Length()-idx)
+		for index,word := range (*text)[idx:] {
+			tmp[index] = string(word)
+		suffixes[idx] = strings.Join(tmp," ")
+		}
+	}
+	return strings.Join(suffixes, "\n")
 }
--- suffixtrees.go
+++ suffixtrees.go
@@ -2,20 +2,27 @@ package textprocessing
 
 import "strings"
 import "sort"
+import "fmt"
+
+type SuffixArrayInterfact interface {
+	Less()
+}
 
 type SuffixArray struct{
         i []int
-        s []string
+        r *Text
 }
 
-func NewSuffixArray(str ... string) (suffixArray *SuffixArray) {
+func NewSuffixArray(str *Text) (suffixArray *SuffixArray) {
         suffixArray = new(SuffixArray)
-        suffixArray.s=str
-        suffixArray.i = make([]int, len(str))
+        suffixArray.r=str
+        suffixArray.i = make([]int, str.Length())
         for idx := range suffixArray.i {
                 suffixArray.i[idx] = idx
         }
         sort.Sort(suffixArray)
+        fmt.Println(suffixArray.i)
+        fmt.Println(suffixArray.r)
         return
 }
 
@@ -24,39 +31,45 @@ func (suffixArray *SuffixArray) Len() (int) {
 }
 
 func (suffixArray *SuffixArray) Less(m, n int) (bool) {
-        midx:=suffixArray.i[m]
-        nidx:=suffixArray.i[n]
-        for{
-                switch {
-                        case suffixArray.s[midx]<suffixArray.s[nidx] :
-                                return true
-                        case suffixArray.s[midx]>suffixArray.s[nidx] :
-                                return false
-                        case suffixArray.s[midx]==suffixArray.s[nidx] :
-                                midx++
-                                nidx++
-                                if nidx == len(suffixArray.i) {
-                                        return false
-                                } else if midx == len(suffixArray.i) {
-                                        return true
-                                }
-                }
-        }
-        panic("unreachable")
+        return suffixArray.r.Less(len(suffixArray.i),suffixArray.i[m],suffixArray.i[n])
 }
 
 func (suffixArray *SuffixArray) Swap(m, n int) {
         suffixArray.i[m], suffixArray.i[n] = suffixArray.i[n], suffixArray.i[m]
 }
 
-func (suffixArray *SuffixArray) SuffixOfLength(length int) (string) {
-	return strings.Join(suffixArray.s[len(suffixArray.s)-length:], " ")
-}
+// func (suffixArray *SuffixArray) SuffixOfLength(length int) (string) {
+// 	return strings.Join(*suffixArray.r[suffixArray.r.Length()-length:], " ")
+// }
 
 func (suffixArray *SuffixArray) String() (string){
-        array := make([]string, len(suffixArray.i))
-        for idx, element := range suffixArray.i {
-                array[idx] = strings.Join(suffixArray.s[element:], " ")
+	suffixes := strings.Split(suffixArray.r.SuffixString(), "\n", -1)
+	array := make([]string, len(suffixes))
+        for idx,val := range suffixArray.i {
+		array[idx] = suffixes[val]
         }
-        return strings.Join(array, "\n")
+        sorted := strings.Join(array, "\n")
+        return string(sorted)
 }
+
+// func (suffixArray *SuffixArray) Less(m, n int) (bool) {
+//         midx:=suffixArray.i[m]
+//         nidx:=suffixArray.i[n]
+//         for{
+//                 switch {
+//                         case suffixArray.s[midx]<suffixArray.s[nidx] :
+//                                 return true
+//                         case suffixArray.s[midx]>suffixArray.s[nidx] :
+//                                 return false
+//                         case suffixArray.s[midx]==suffixArray.s[nidx] :
+//                                 midx++
+//                                 nidx++
+//                                 if nidx == len(suffixArray.i) {
+//                                         return false
+//                                 } else if midx == len(suffixArray.i) {
+//                                         return true
+//                                 }
+//                 }
+//         }
+//         panic("unreachable")
+// }
