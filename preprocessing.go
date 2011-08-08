2d4f1870dc787996f29a3f20b6798e8a2eda049b
Lea Frermann <l1@frermann.de>
2011-08-08 18:36:41 +0200

    Fixed bug in NewText: now returns empty Text for empty document (before, because of weird behavior of split, a Text with an empty word was returned)

--- preprocessing.go
+++ preprocessing.go
@@ -11,23 +11,26 @@ type Word string
 type Text []Word
 
 func NewTextFile(filename string) (text *Text) {
-    rawTextByte,_ := ioutil.ReadFile(filename)
-    text = NewText(string(rawTextByte))
-    return
+	rawTextByte,_ := ioutil.ReadFile(filename)
+	text = NewText(string(rawTextByte))
+	return
 }
 
 func NewText(str string) (text *Text) {
-    const punct = "[?!.,;:^/\\<>\\(\\)\"]*"
-    reStrip := regexp.MustCompile(punct+"([ \t\n]+"+punct+")+")
-    str = reStrip.ReplaceAllString(str, " ")
-    str = strings.Trim(str, " \n\t?!.,;:^/\\<>()\"")
-    textStringSlice := strings.Split(str, " ", -1)
-    slice := make(Text,len(textStringSlice))
-    text = &slice
-    for idx,word := range textStringSlice {
-        (*text)[idx]=Word(strings.ToLower(word))
-        }
-    return
+	const punct = "[?!.,;:^/\\<>\\(\\)\"]*"
+	reStrip := regexp.MustCompile(punct+"([ \t\n]+"+punct+")+")
+	str = reStrip.ReplaceAllString(str, " ")
+	str = strings.Trim(str, " \n\t?!.,;:^/\\<>()\"")
+	var textStringSlice []string
+	if str != "" {
+		textStringSlice = strings.Split(str, " ", -1)
+	}
+	slice := make(Text,len(textStringSlice))
+	text = &slice
+	for idx,word := range textStringSlice {
+		(*text)[idx]=Word(strings.ToLower(word))
+	}
+	return
 }
 
 func (text *Text) Length() (int) {
@@ -37,23 +40,23 @@ func (text *Text) Length() (int) {
 func (suffixArray *SuffixArray) Less(m, n int) (bool) {
 	text := suffixArray.r
 	m,n=suffixArray.i[m],suffixArray.i[n]
-        for{
-                switch {
-                        case text.Less(m,n):
-                                return true
-                        case text.Less(n,m) :
-                                return false
-                        default :
-                                m++
-                                n++
-                                if n == text.Length() {
-                                        return false
-                                } else if m == text.Length() {
-                                        return true
-                                }
-                }
-        }
-        panic("unreachable")
+	for{
+		switch {
+			case text.Less(m,n):
+				return true
+			case text.Less(n,m) :
+				return false
+			default :
+				m++
+				n++
+				if n == text.Length() {
+					return false
+				} else if m == text.Length() {
+					return true
+				}
+		}
+	}
+	panic("unreachable")
 }
 
 func (text Text) Less(m, n int) (bool) {
@@ -65,5 +68,5 @@ func (text *Text) SuffixString(length int) (string) {
 	for index,word := range (*text)[text.Length()-length:] {
 		suffix[index] = string(word)
 	}
-  	return strings.Join(suffix, " ")
+	return strings.Join(suffix, " ")
 }
